57f53095e1051ead44a3e3054c6c94b1efbb2c7e
JavadBargamadi <53382480+javadlbargamadi@users.noreply.github.com>
2019-11-26 20:51:17 +0330

    start building

--- app/src/main/java/com/example/themoviedbpro/MainActivity.kt
+++ app/src/main/java/com/example/themoviedbpro/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.themoviedbpro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//--------------------------------------------------------------------------------------------------
+
+    /**
+     * start search activity from main activity
+     */
+    private fun startSearchActivity() {
+        intent = Intent(this, SearchActivity::class.java)
+        startActivity(intent)
+    }
+
+//--------------------------------------------------------------------------------------------------
+
+    /**
+     * start saved activity from main activity
+     */
+    private fun startSavedActivity() {
+        intent = Intent(this, SavedActivity::class.java)
+        startActivity(intent)
+    }
+
+//--------------------------------------------------------------------------------------------------
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSaved.setOnClickListener { startSavedActivity() }
+        btnSearch.setOnClickListener { startSearchActivity() }
     }
 }
---
+++ app/src/main/java/com/example/themoviedbpro/SavedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.themoviedbpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SavedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saved)
+    }
+}
